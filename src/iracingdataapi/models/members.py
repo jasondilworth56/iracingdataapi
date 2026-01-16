d0cda0408d3b8cbaa7aba420455d000998da0631
JDCodes <twitchjdf@gmail.com>
2026-01-16 10:27:30 +0100

    Change logo_url type to support None value

--- src/iracingdataapi/models/members.py
+++ src/iracingdataapi/models/members.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any
 
 from pydantic import BaseModel, Field
 from typing_extensions import Literal
@@ -208,7 +208,7 @@ class MemberRecentEvent(BaseModel):
     event_name: str
     event_type: str
     finish_position: int
-    logo_url: Optional[str] = None
+    logo_url: str | None = None
     percent_rank: int
     simsession_type: int
     start_time: str
